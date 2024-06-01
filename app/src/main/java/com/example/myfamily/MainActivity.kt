b83c715491e4d89d79e5034dcf4ede6b68cb163e
Ahtisham Ali <raiahtisham76@gmail.com>
2024-06-02 01:51:44 +0500

    init project

---
+++ app/src/main/java/com/example/myfamily/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.myfamily
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val bottomBar = findViewById<BottomNavigationView>(R.id.bottom_bar)
+
+        bottomBar.setOnItemSelectedListener { menuItem ->          // It will be called when we will click any icon in bottom navigation
+            when (menuItem.itemId) {
+                R.id.nav_guard -> {
+
+                    inflateFragment(GuardFragment.newInstance())
+
+                }
+                R.id.nav_home -> {
+
+                    inflateFragment(HomeFragment.newInstance())
+
+                }
+                R.id.nav_home -> {
+
+                    inflateFragment(DashboardFragment.newInstance())
+
+                }
+                R.id.nav_home -> {
+
+                    inflateFragment(ProfileFragment.newInstance())
+
+                }
+            }
+
+            true      // Selection in the navigation bar will be shown by true
+        }
+    }
+
+    private fun inflateFragment(newInstance: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, newInstance)
+        transaction.commit()
+    }
+}
